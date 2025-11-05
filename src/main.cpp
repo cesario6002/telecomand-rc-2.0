0e3928cf0f99f00366db308313499972d5afc92e
Césario <149043625+cesario6002@users.noreply.github.com>
2025-11-05 22:01:14 +0100

    v2

--- src/main.cpp
+++ src/main.cpp
@@ -29,21 +29,23 @@ void setup() {
   radio.begin();
   radio.openWritingPipe(0xF0F0F0F0E1LL);  // Adresse du récepteur
   radio.setPALevel(RF24_PA_HIGH);
-  radio.setDataRate(RF24_1MBPS);
+  radio.setDataRate(RF24_250KBPS);
   Serial.println("Émetteur prêt !");
 }
 
 void loop() {
   
   //lecture des valeurs des joysticks
-  envoi.gaz = map(analogRead(35), 0, 4095, 0, 225); //joystick 1
-  envoi.ailleron = map(analogRead(12), 0, 4095, 45, 135); //joystick 2
-  envoi.prof = map(analogRead(13), 0, 4095, 0, 180); //joystick 2
-  envoi.gouv = map(analogRead(33), 0, 4095, 0, 180); //joystick 2
+  envoi.gaz = map(analogRead(35), 0, 4095, 1000, 2000); //joystick 1
+  envoi.ailleron = map(analogRead(33), 0, 4095, 45, 135); //joystick 2
+  envoi.prof = map(analogRead(13), 0, 4095, 0, 120);
+
+  envoi.gouv = map(analogRead(12), 0, 4095, 180, 0); //joystick 2
   delay(10);
   // Message à envoyer
   radio.write(&envoi, sizeof(envoi));
   verifie = radio.write(&envoi, sizeof(envoi));
+  Serial.println(envoi.prof);
 
   if (!verifie) {
     Serial.println("Erreur d'envoi !");
