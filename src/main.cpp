ff998d8a3e695c9a8dc5651201883299f7bf4fce
Césario <149043625+cesario6002@users.noreply.github.com>
2025-12-03 03:42:03 +0100

    ajout des volets

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,16 @@ int verifie;
 RF24 radio(21, 5);  // CE, CSN
 
       //joystick 1
-        //int x1 = 34;
+        //int x1 = 33;
         //int y1 = 35;
       //joystick 2
         //int x2 = 12;
         //int y2 = 13;
+      //joystick 3
+        //int x3 = 14; volet
+      //Switches
+        //int sw1 = 32; turbo
+        //int sw2 = 34; largeur
 
 //initialisation des variables paquet a envoyer
 
@@ -22,6 +27,9 @@ struct Commande
   int ailleron;
   int prof;
   int gouv;
+  int volet;
+  int turbo;
+  int largeur;
 };
 Commande envoi;
 void setup() {
@@ -38,9 +46,11 @@ void loop() {
   //lecture des valeurs des joysticks
   envoi.gaz = map(analogRead(35), 0, 4095, 1000, 2000); //joystick 1
   envoi.ailleron = map(analogRead(33), 0, 4095, 45, 135); //joystick 2
-  envoi.prof = map(analogRead(13), 0, 4095, 0, 120);
-
-  envoi.gouv = map(analogRead(12), 0, 4095, 180, 0); //joystick 2
+  envoi.prof = map(analogRead(13), 0, 4095, 45, 135);
+  envoi.gouv = map(analogRead(12), 0, 4095, 135, 45); //joystick 2
+  envoi.volet = map(analogRead(14), 0, 4095, 0, 70); //joystick 3
+  envoi.turbo = digitalRead(32); //switch 1
+  envoi.largeur = digitalRead(34); //switch 2
   delay(10);
   // Message à envoyer
   radio.write(&envoi, sizeof(envoi));
@@ -50,5 +60,5 @@ void loop() {
   if (!verifie) {
     Serial.println("Erreur d'envoi !");
   }
-  delay(10);  // Attendre 50 ms avant le prochain envoi
+  delay(10);  // Attendre 10 ms avant le prochain envoi
 }
